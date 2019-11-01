b4d3cc962e7f8172f9d93f82a337246473309829
Lawrence Gandhar <lawrence-gandhar@users.noreply.github.com>
2019-11-01 20:14:28 +0530

    initial template upload
    index page template and related files upload

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    template_name = 'app/registration/index.html'
+    return render(request, template_name, {})
