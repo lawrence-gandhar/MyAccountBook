f9047f9d5efd6833fc026753e0b09809ca068ab5
Lawrence Gandhar <lawrence-gandhar@users.noreply.github.com>
2019-11-01 21:51:23 +0530

    login page template

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from . import views
+from django.urls import path, include, re_path
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+from app.views import *
 
 urlpatterns = [
-    path('', views.index, name = 'home'),
+    path('', index, name = 'home'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'app/registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'app/registration/logout.html'), name = 'logout'),
+    re_path(r'^accounts/*', RedirectView.as_view(pattern_name='login', permanent=True)),
 ]
