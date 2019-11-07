2ebc5d2c02073743b16dc54f3d11cf05a7acb12b
Lawrence Gandhar <lawrence-gandhar@users.noreply.github.com>
2019-11-07 05:59:42 +0530

    contacts_model created

--- app/models/contacts_model.py
+++ app/models/contacts_model.py
@@ -9,17 +9,22 @@ class Contacts(models.Model):
 
     IS_ACTIVE = ((True, 'YES'), (False, 'NO'))
 
+    is_customer = models.BooleanField(
+        db_index = True,
+        choices = IS_ACTIVE,
+        default = False,
+    )
+
+    is_vendor = models.BooleanField(
+        db_index = True,
+        choices = IS_ACTIVE,
+        default = False,
+    )
+
     contact_name = models.CharField(
         max_length = 250,
         blank = False,
         null = False,
-        db_index = True,
-    )
-
-    contact_email = models.CharField(
-        max_length = 250,
-        blank = True,
-        null = True,
         db_index = True,
     )
 
@@ -29,6 +34,11 @@ class Contacts(models.Model):
         default = True,
     )
 
+    notes = models.TextField(
+        blank = True,
+        null = True,
+    )
+    
     created_on = models.DateTimeField(
         auto_now = True,
         db_index = True,
@@ -40,7 +50,7 @@ class Contacts(models.Model):
         null = True,
     )
 
-    class META:
+    class Meta:
         verbose_name_plural = 'contacts_tbl'
 
 
@@ -51,6 +61,15 @@ class Contacts(models.Model):
 class Contacts_Email(models.Model):
 
     EMAIL_CHOICES = ((True, 'Yes'),(False, 'No'))
+
+    PAYMENT_DAYS = (
+        (1, 'On Due Date'),
+        (2, '10 Days'),
+        (3, '20 Days'),
+        (4, '30 Days'),
+        (5, '60 Days'),
+        (6, '90 Days'),
+    )
 
     contact = models.ForeignKey(
         Contacts, 
@@ -64,16 +83,52 @@ class Contacts_Email(models.Model):
         db_index = True,
     )
 
-    is_official = models.Model(
+    is_official = models.BooleanField(
         db_index = True,
         choices = EMAIL_CHOICES,
         default = True,
     )
 
-    is_primary = models.Model(
+    is_primary = models.BooleanField(
         db_index = True,
         choices = EMAIL_CHOICES,
         default = True,
+    )
+
+    pan = models.CharField(
+        max_length = 10,
+        db_index = True,
+        null = False,
+        blank = False,
+    )
+
+    gstin = models.CharField(
+        max_length = 100,
+        db_index = True,
+        null = True,
+        blank = True,
+    )
+
+    tds = models.DecimalField(
+        db_index = True,
+        null = True,
+        blank = True,
+        max_digits = 20, 
+        decimal_places = 2
+    )
+
+    invoice_terms = models.IntegerField(
+        null = True,
+        blank = True,
+        db_index = True,
+        choices = PAYMENT_DAYS,
+    )
+
+    bills_terms = models.IntegerField(
+        null = True,
+        blank = True,
+        db_index = True,
+        choices = PAYMENT_DAYS,
     )
 
     created_on = models.DateTimeField(
@@ -81,5 +136,126 @@ class Contacts_Email(models.Model):
         db_index = True,
     )
 
-    class META:
+    updated_on = models.DateTimeField(
+        auto_now = False,
+        db_index = True,
+        null = True,
+    )
+
+    class Meta:
         verbose_name_plural = 'contacts_email_tbl'
+
+
+#**************************************************************************
+#   ADDRESSES OF CONTACTS
+#   A CONTACT CAN HAVE MULTIPLE ADDRESSES
+#**************************************************************************
+
+class Contact_Addresses(models.Model):
+
+    ADDRESS_CHOICES = ((True, 'Yes'),(False, 'No'))
+
+    contact = models.ForeignKey(
+        Contacts, 
+        on_delete = models.CASCADE, 
+        db_index = True
+    )
+
+    contact_name = models.CharField(
+        max_length = 250,
+        blank = False,
+        null = False,
+        db_index = True,
+    )
+
+    is_billing_address = models.BooleanField(
+        db_index = True,
+        choices = ADDRESS_CHOICES,
+        default = False,
+    ) 
+
+    is_shipping_address = models.BooleanField(
+        db_index = True,
+        choices = ADDRESS_CHOICES,
+        default = False,
+    )
+
+    same_billing_shipping_address = models.BooleanField(
+        db_index = True,
+        choices = ADDRESS_CHOICES,
+        default = False,
+    )
+
+    created_on = models.DateTimeField(
+        auto_now = True,
+        db_index = True,
+    )
+
+    updated_on = models.DateTimeField(
+        auto_now = False,
+        db_index = True,
+        null = True,
+    )
+
+    class Meta:
+        verbose_name_plural = 'contacts_address_details_tbl'
+
+
+#**************************************************************************
+#   CONTACT'S ACCOUNT DETAILS
+#**************************************************************************
+
+class Contact_Account_Details(models.Model):
+    contact = models.ForeignKey(
+        Contacts, 
+        on_delete = models.CASCADE, 
+        db_index = True
+    )
+
+    account_number = models.BigIntegerField(
+        null = True,
+        db_index = True,
+    )
+
+    account_holder_name = models.CharField(
+        max_length = 250,
+        blank = True,
+        null = True,
+        db_index = True,
+    )
+
+    ifsc_code = models.CharField(
+        max_length = 20,
+        null = True,
+        blank = True,
+        db_index = True,
+    )
+
+    bank_name = models.CharField(
+        max_length = 250,
+        db_index = True,
+        null = True,
+        blank = True,
+    )
+
+    bank_branch_name = models.CharField(
+        max_length = 250,
+        db_index = True,
+        null = True,
+        blank = True,
+    )
+
+    created_on = models.DateTimeField(
+        auto_now = True,
+        db_index = True,
+    )
+
+    updated_on = models.DateTimeField(
+        auto_now = False,
+        db_index = True,
+        null = True,
+    )
+
+    class Meta:
+        verbose_name_plural = 'contacts_account_details_tbl'
+
