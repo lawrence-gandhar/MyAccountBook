d356589afb778cab246f9ea0e9b526b6e9aff313
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2019-11-08 21:07:38 +0530

    recent changes

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from app.models.contacts_model import Contacts as C
 # Register your models here.
+
+@admin.register(C)
+class C(admin.ModelAdmin):
+    model = C
+
