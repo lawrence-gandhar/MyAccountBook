081635b31f15fc4870909422adcffe79922548d6
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2019-12-20 11:53:48 +0530

    js component

--- app/static/custom_files/js/contacts.js
+++ app/static/custom_files/js/contacts.js
@@ -43,10 +43,25 @@ function check_app_id(elem){
             if(data["ret"] == 0){ 
                 alert('Invalid Application ID');
                 $("#id_app_id_check").empty().append('<i class="fa fa-fw fa-times" style="color: #ff0000;"></i>');
+                $("#id_app_id_button").hide();
             }
             if(data["ret"]=='1'){
                 $("#id_app_id_check").empty().append('<i class="fa fa-fw fa-check" style="color: #11640b;"></i>');
+                $("#id_app_id_button").show();
             }
         });
+    }
+}
+
+
+//******************************************* 
+//  CHECK IF APPLICATION ID EXISTS 
+//********************************************
+
+function send_contact_request(){
+    var app_id = $("#id_app_id").val();
+
+    if(app_id!=""){
+        $.post()
     }
 }
