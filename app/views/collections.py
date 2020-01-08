cd570160c5ffc21c2407bccd41b48b52f49812eb
Lawrence Gandhar <lawrence-gandhar@users.noreply.github.com>
2020-01-09 00:58:53 +0530

    Update collections.py

--- app/views/collections.py
+++ app/views/collections.py
@@ -32,3 +32,10 @@ class Collections(View):
     #
     def get(self, request):        
         return render(request, self.template_name, self.data)
+
+
+    #
+    #
+    #
+    def post(self, request):
+        pass
