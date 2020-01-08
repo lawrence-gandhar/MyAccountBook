245c4b34c0a6de1060047fa6924339ac6fb46849
Lawrence Gandhar <lawrence-gandhar@users.noreply.github.com>
2020-01-08 22:24:15 +0530

    Create collects_model.py

---
+++ app/models/collects_model.py
@@ -0,0 +1,106 @@
+from django.db import models
+from django.contrib.auth.models import User
+from app.models.contacts_model import Contacts
+
+class Collects_Model(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete = models.SET_NULL
+        db_index = True,
+        blank = True,
+        null = True,
+    )
+
+    contact = models.ForeignKey(
+        Contacts,
+        on_delete = models.SET_NULL
+        db_index = True,
+        blank = True,
+        null = True,
+    )
+
+    
+
+#=====================================================================
+# COLLECTION DUE DATES & REMINDERS
+#=====================================================================
+class Collection_Due_model(models.Model):
+
+    collects = models.ForeignKey(
+        Collects_Model,
+        on_delete = models.CASCADE,
+        blank = True,
+        null = True,
+        db_index = True,
+    )
+
+    collection_due_date = models.DateTimeField(
+        null = True,
+        blank = True,
+        db_index = True,
+    )
+
+    amount = models.FloatField(
+        blank = True,
+        null = True,
+        db_index = True,
+    )
+
+#=====================================================================
+# MULTIPLE COLLECTION METHODS
+#=====================================================================
+class Collection_model(models.Model):
+
+    PAYMENT_MODES = (
+        (1, 'Cash'),
+        (2, 'Cheque'),
+        (3, 'Demand Draft'),
+        (4, 'Payment Gateway'),
+    )
+
+    COLLECTION_STATUS = (
+        (1, 'Collection Expected'),
+        (2, 'Still Collecting'),
+        (3, 'Collected'),
+    )
+
+    collects = models.ForeignKey(
+        Collects_Model,
+        on_delete = models.CASCADE,
+        blank = True,
+        null = True,
+        db_index = True,
+    )
+
+    collection_date = models.DateTimeField(
+        null = True,
+        blank = True,
+        db_index = True,
+    )
+
+    amount = models.FloatField(
+        blank = True,
+        null = True,
+        db_index = True,
+    )
+
+    payment_type = models.IntegerField(
+        null = True,
+        blank = True,
+        db_index = True,
+        choices = PAYMENT_MODES,
+    )
+
+    collection_status = models.IntegerField(
+        null = True,
+        blank = True,
+        db_index = True,
+        choices = COLLECTION_STATUS,
+    )
+
+    collection_date = models.DateTimeField(
+        null = True,
+        blank = True,
+        db_index = True,
+    )
