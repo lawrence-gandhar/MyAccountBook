ee525e59346f8446e4d84cd98f7a45302a766af5
Lawrence Gandhar <lawrence-gandhar@users.noreply.github.com>
2020-01-16 05:05:26 +0530

    new updates

--- app/static/custom_files/js/design_template.js
+++ app/static/custom_files/js/design_template.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
 
+    $("button.color-button").on('click', function(){
+        var x = $(this).text();
+
+        var backgrounds = {
+            "1" : "template_header",
+            "2" : "template_header_orange",
+            "3" : "template_header_blue",
+        };
+
+        $("#invoice_template_body").find(".card-header").removeClass().addClass("card-header").addClass(backgrounds[x]);
+        
+
+    });
+
+
+
 });
 
