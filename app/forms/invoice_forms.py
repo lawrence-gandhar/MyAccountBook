f3b63eb4c9d702b2fb720e188248dcb562df008d
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2020-01-18 07:45:27 +0530

    design invoice update

--- app/forms/invoice_forms.py
+++ app/forms/invoice_forms.py
@@ -10,21 +10,32 @@ class InvoiceDesignerForm(ModelForm):
 
     class Meta:
 
+        IS_ACTIVE = ((True, 'YES'), (False, 'NO'))
+
+        TEMPLATE_DESIGN = (
+            (1, 'GREEN TEMPLATE'),
+            (2, 'ORANGE TEMPLATE'),
+            (3, 'BLUE TEMPLATE'),
+            (4, 'WHITE TEMPLATE'),
+            (5, 'GREY TEMPLATE'),
+            (6, 'CUSTOM TEMPLATE'),
+        )
+
         model = Invoice_Templates
 
         fields = (
-            'template_name', 'design_number', 'logo', 'header_bgcolor', 'header_fgcolor', 'other_design_colors',
-            'is_active',
+            'template_name', 'design_number', 'logo', 'header_bgcolor', 'header_fgcolor', 
+            'other_design_colors', 'is_active',
         )
 
         widgets = {
             'template_name' : TextInput(attrs={'class':'form-control input-sm', 'required':'true'}),
-            'design_number' : TextInput(attrs={'hidden':'true',}),
+            'design_number' : Select(attrs={'class': 'form-control input-sm',}, choices = TEMPLATE_DESIGN),
             'logo' : FileInput(attrs={'class':'form-control input-sm',}),
             'header_bgcolor' : TextInput(attrs={'class':'form-control input-sm','type':'color'}),
             'header_fgcolor' : TextInput(attrs={'class':'form-control input-sm','type':'color'}),
             'other_design_colors' : TextInput(attrs={'class':'form-control input-sm','type':'color'}),
-            'is_active' : Select(attrs={'class':'form-control input-sm',)
+            'is_active' : Select(attrs={'class':'form-control input-sm',}, choices = IS_ACTIVE),
         }
 
 
--- app/static/custom_files/js/design_template.js
+++ app/static/custom_files/js/design_template.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
 
+    var backgrounds = {
+        "1" : ["template_header", "template_body_green", "th_green"],
+        "2" : ["template_header_orange", "template_body_orange", "th_orange"],
+        "3" : ["template_header_blue", "template_body_blue", "th_blue"],
+        "4" : ["template_header_white", "template_body_white", "th_white"],
+        "5" : ["template_header_grey", "template_body_grey", "th_grey"],
+    };
+
     $("button.color-button").on('click', function(){
         var x = $(this).text();
 
-        var backgrounds = {
-            "1" : ["template_header", "template_body_green", "th_green"],
-            "2" : ["template_header_orange", "template_body_orange", "th_orange"],
-            "3" : ["template_header_blue", "template_body_blue", "th_blue"],
-            "4" : ["template_header_white", "template_body_white", "th_white"],
-            "5" : ["template_header_grey", "template_body_grey", "th_grey"],
-        };
+        $("div.hidden_div").css('display','none !important');
 
         $("#invoice_template_body").find(".card-header").removeClass().addClass("card-header").addClass(backgrounds[x][0]);
         $("#template_body").removeClass().addClass("card-body").addClass(backgrounds[x][1]);
         $("#address_bar").removeClass().addClass(backgrounds[x][2])
+
+        $("#id_design_number").val(x);
+            
     });
+
+    $("select#id_design_number").on('change', function(){
+        var x = $(this).val();
+
+        if(x == 6){
+            $("div.hidden_div").addClass('d-table-row');
+        }else{
+            $("div.hidden_div").removeClass('d-table-row');
+        }
+    })
 
 
 
