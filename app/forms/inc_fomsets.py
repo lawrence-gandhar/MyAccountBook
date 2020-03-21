d921a3a6abde3430da29aaa9bd4b8385eb41e614
lawrence-gandhar <lawrence_gandhar@yahoo.com>
2020-03-21 21:14:24 +0530

    updated

--- app/forms/inc_fomsets.py
+++ app/forms/inc_fomsets.py
@@ -18,8 +18,8 @@ AddressFormset = inlineformset_factory(contacts_model.Contacts, users_model.User
         'state' : Select(attrs={'class':'form-control input-sm',}, choices = country_list.STATE_LIST_CHOICES),
         'country' : Select(attrs={'class':'form-control input-sm',}, choices = country_list.COUNTRIES_LIST_CHOICES),
         'pincode' : TextInput(attrs={'class':'form-control input-sm',}),
-        'is_billing_address' : Select(attrs={'class':'form-control input-sm',}, choices = user_constants.IS_TRUE, ),
-        'is_shipping_address' : Select(attrs={'class':'form-control input-sm',}, choices = user_constants.IS_TRUE, ),
+        'is_billing_address' : CheckboxInput(attrs={'class':'form-control input-sm',},),
+        'is_shipping_address' : CheckboxInput(attrs={'class':'form-control input-sm',},),
     }
 )
 
